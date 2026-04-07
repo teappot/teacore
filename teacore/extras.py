c5846d5f38684c9a3dd91128fa9e67c4f1153fa3
Walter Riedemann <walter@conway.cl>
2026-04-06 23:06:40 -0400

    Cambio en el form de account

--- teacore/extras.py
+++ teacore/extras.py
@@ -17,7 +17,6 @@ def hx_redirect(to, *args, permanent=False, **kwargs):
 
 def async_send_mail(subject, message, recipient_list, from_email=settings.EMAIL_DEFAULT_SENDER, auth_user=settings.EMAIL_HOST_USER):
 
-    """
     import threading
     threading.Thread (
         target=send_mail,
@@ -29,16 +28,6 @@ def async_send_mail(subject, message, recipient_list, from_email=settings.EMAIL_
             'auth_user': auth_user,
             'recipient_list': recipient_list
     }).start()
-    """
-    res = send_mail(
-        subject,
-        message,
-        from_email,
-        recipient_list,
-        auth_user=auth_user,
-        html_message=message,
-    )
-    print("res", res)
 
 def csrf_failure(request, reason=""):
     """
